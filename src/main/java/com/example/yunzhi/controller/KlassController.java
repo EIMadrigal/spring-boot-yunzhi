4e44c300b30cffc50fe663326fa78af707c76c4c
EI Madrigal <andrew_ren@163.com>
2024-12-14 16:05:54 +0800

    add class finish

--- src/main/java/com/example/yunzhi/controller/KlassController.java
+++ src/main/java/com/example/yunzhi/controller/KlassController.java
@@ -5,9 +5,13 @@ import com.example.yunzhi.repository.KlassRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,6 +29,12 @@ public class KlassController {
     public List<Klass> getAll(@RequestParam String name) {
         logger.info(name);
         return this.klassRepository.findAllByNameContains(name);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody Klass klass) {
+        klassRepository.save(klass);
     }
 
 }
---
+++ src/main/java/com/example/yunzhi/repository/TeacherRepository.java
@@ -0,0 +1,8 @@
+package com.example.yunzhi.repository;
+
+import com.example.yunzhi.model.Teacher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeacherRepository extends CrudRepository<Teacher, Long> {
+
+}
